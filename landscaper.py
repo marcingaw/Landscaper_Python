323ca39fcd081c736fa31750eaf5d42730a0ee1c
Marcin Gawlik <marcin.gawlik@windowslive.com>
2020-12-05 18:35:21 +0100

    Extracted the procedure to generate terrain into a separate source file.
    Extracted the procedure to generate terrain into a separate source file.

--- landscaper.py
+++ landscaper.py
@@ -1,126 +1,9 @@
-import random
+import terraingen
 import turtle
 
-# 'map_size' must be a power of two because in each step of the Diamond Square
-# algorithm the step size is divided by 2. The algorithm ends when the step is
-# 2 (so in that step the remaining pixels between generated pixels are filled).
-# Returns a list of 'map_size+1' lists with 'map_size+1' floats each.
-# 'complexity' tells how many starting points should be randomized, 0 means
-# corners only, 1 means 9 points, 2 means 25 etc.
-def PrepareLandscape(map_size, complexity):
-
-    # Calculate average elevation of corners of a square surrounding the given point
-    # for the diamond step.
-    def DiamondAvgRnd(lndscp, row, col, size, rnd_div):
-        
-        if lndscp[row][col] is not None:
-            return lndscp[row][col]
-        return ((lndscp[row - size][col - size] + lndscp[row - size][col + size] +
-                 lndscp[row + size][col - size] + lndscp[row + size][col + size]) / 4.0 +
-                (random.random() - 0.5) / rnd_div)
-
-    # Calculate average elevation of corners of a diamond surrounding the given
-    # point for the square step - this needs special handling for points along the
-    # boundaries of the landscape.
-    def SquareAvgRnd(lndscp, row, col, size, rnd_div):
-        
-        if lndscp[row][col] is not None:
-            return lndscp[row][col]
-
-        point_sum = 0.0
-        point_cnt = 0.0
-
-        if row >= size:
-            point_sum = point_sum + lndscp[row - size][col]
-            point_cnt = point_cnt + 1.0
-
-        if row < len(lndscp) - size:
-            point_sum = point_sum + lndscp[row + size][col]
-            point_cnt = point_cnt + 1.0
-
-        if col >= size:
-            point_sum = point_sum + lndscp[row][col - size]
-            point_cnt = point_cnt + 1.0
-
-        if col < len(lndscp[row]) - size:
-            point_sum = point_sum + lndscp[row][col + size]
-            point_cnt = point_cnt + 1.0
-
-        return point_sum / point_cnt + (random.random() - 0.5) / rnd_div
-
-    # This will be initialized to be a 2D array indexed 0...MAP_SIZE.
-    landscape = []
-
-    for r in range(map_size + 1):
-        landscape.append([])
-        
-        for c in range(map_size + 1):
-            landscape[r].append(None)
-            
-    # Pick random values in the starting points.
-    start_step = MAP_SIZE // (2 ** complexity)
-
-    for r in range(0, map_size + 1, start_step):
-
-        for c in range(0, map_size + 1, start_step):
-            landscape[r][c] = random.random()
-
-    min_pixel = landscape[0][0]
-    max_pixel = landscape[0][0]
-
-    # Now let's do the Diamond Square trick...
-    step_size = map_size
-    rnd_div = 2.0
-    
-    while step_size > 1:
-        print(f'current step size: {step_size}')
-        half_step = step_size // 2
-        # Diamond.
-        
-        for r in range(0, map_size, step_size):
-            
-            for c in range(0, map_size, step_size):
-                landscape[r + half_step][c + half_step] = DiamondAvgRnd(
-                    landscape, r + half_step, c + half_step, half_step, rnd_div)
-                min_pixel = min(min_pixel, landscape[r + half_step][c + half_step])
-                max_pixel = max(max_pixel, landscape[r + half_step][c + half_step])
-
-
-        # Square.
-        for r in range(0, map_size + 1, step_size):
-            
-            for c in range(0, map_size + 1, step_size):
-                
-                if r < map_size:
-                    landscape[r + half_step][c] = SquareAvgRnd(
-                        landscape, r + half_step, c, half_step, rnd_div)
-                    min_pixel = min(min_pixel, landscape[r + half_step][c])
-                    max_pixel = max(max_pixel, landscape[r + half_step][c])
-                    
-                if c < map_size:
-                    landscape[r][c + half_step] = SquareAvgRnd(
-                        landscape, r, c + half_step, half_step, rnd_div)
-                    min_pixel = min(min_pixel, landscape[r][c + half_step])
-                    max_pixel = max(max_pixel, landscape[r][c + half_step])
-
-        # Prepare the next step.
-        step_size = half_step
-        rnd_div = 2.0 * rnd_div
-
-    delta = max_pixel - min_pixel
-
-    if delta == 0.0:  # rather not realistic, unless the map size is 1
-        delta = 1.0
-
-    for r in range(map_size + 1):
-
-        for c in range(map_size + 1):
-            landscape[r][c] = (landscape[r][c] - min_pixel) / delta
-
-    return landscape
-
-MAP_SIZE = 512
-landscape = PrepareLandscape(MAP_SIZE, 1)
+MAP_SIZE_EXP = 9
+landscape = terraingen.PrepareLandscape(MAP_SIZE_EXP, 1)
+MAP_SIZE = 2**MAP_SIZE_EXP
 
 # for r in range(MAP_SIZE + 1):
 #     for c in range(MAP_SIZE + 1):
@@ -128,6 +11,7 @@ landscape = PrepareLandscape(MAP_SIZE, 1)
 #     print()
 
 def ColorForPixel(p):
+    
     if p < 0.1:
         return (0.0, 0.0, 0.5)
     if p < 0.2:
@@ -146,6 +30,7 @@ def ColorForPixel(p):
         return (0.7, 0.7, 0.0)
     if p < 0.9:
         return (0.7, 0.3, 0.0)
+    
     return (0.5, 0.0, 0.0)
 
 turtle.colormode(1.0)
@@ -155,7 +40,7 @@ turtle.hideturtle()
 # Set to 1 to draw the map at the highest resolution. Otherwise it will
 # gradually repaint it with higher and higher resolutions, but overall it will
 # take much more time.
-step = MAP_SIZE // 16
+step = 1 #MAP_SIZE // 16
 
 while step > 0:
     print(f'drawing block size: {step}')
---
+++ terraingen.py
@@ -0,0 +1,120 @@
+import random
+
+def PrepareLandscape(map_size_exp, complexity):
+    '''Generates a landscape as a square array of floats 0.0...1.0.
+
+    map_size-exp - the landscape will be of the size 2**map_size+1
+    complexity - 0, 1 or 2 - there will be, resp., 4, 9 or 25 initial points on the map
+    returns a square array representing the terrain, values are floats 0.0...1.0'''
+
+    def DiamondAvgRnd(lndscp, row, col, size, rnd_div):
+        '''Calculate average elevation of corners of a square surrounding the
+        given point for the diamond step.'''
+        
+        if lndscp[row][col] is not None:
+            return lndscp[row][col]
+        
+        return ((lndscp[row - size][col - size] + lndscp[row - size][col + size] +
+                 lndscp[row + size][col - size] + lndscp[row + size][col + size]) / 4.0 +
+                (random.random() - 0.5) / rnd_div)
+
+    def SquareAvgRnd(lndscp, row, col, size, rnd_div):
+        '''Calculate average elevation of corners of a diamond surrounding the
+        given point for the square step - this needs special handling for points
+        along the boundaries of the landscape.'''
+        
+        if lndscp[row][col] is not None:
+            return lndscp[row][col]
+
+        point_sum = 0.0
+        point_cnt = 0.0
+
+        if row >= size:
+            point_sum = point_sum + lndscp[row - size][col]
+            point_cnt = point_cnt + 1.0
+
+        if row < len(lndscp) - size:
+            point_sum = point_sum + lndscp[row + size][col]
+            point_cnt = point_cnt + 1.0
+
+        if col >= size:
+            point_sum = point_sum + lndscp[row][col - size]
+            point_cnt = point_cnt + 1.0
+
+        if col < len(lndscp[row]) - size:
+            point_sum = point_sum + lndscp[row][col + size]
+            point_cnt = point_cnt + 1.0
+
+        return point_sum / point_cnt + (random.random() - 0.5) / rnd_div
+
+    map_size = 2**map_size_exp
+    
+    # This will be initialized to be a 2D array indexed 0...map_size.
+    landscape = []
+
+    for r in range(map_size + 1):
+        landscape.append([])
+        
+        for c in range(map_size + 1):
+            landscape[r].append(None)
+            
+    # Pick random values in the starting points.
+    start_step = map_size // (2 ** complexity)
+
+    for r in range(0, map_size + 1, start_step):
+
+        for c in range(0, map_size + 1, start_step):
+            landscape[r][c] = random.random()
+
+    min_pixel = landscape[0][0]
+    max_pixel = landscape[0][0]
+
+    # Now let's do the Diamond Square trick...
+    step_size = map_size
+    rnd_div = 2.0
+    
+    while step_size > 1:
+        half_step = step_size // 2
+        # Diamond.
+        
+        for r in range(0, map_size, step_size):
+            
+            for c in range(0, map_size, step_size):
+                landscape[r + half_step][c + half_step] = DiamondAvgRnd(
+                    landscape, r + half_step, c + half_step, half_step, rnd_div)
+                min_pixel = min(min_pixel, landscape[r + half_step][c + half_step])
+                max_pixel = max(max_pixel, landscape[r + half_step][c + half_step])
+
+
+        # Square.
+        for r in range(0, map_size + 1, step_size):
+            
+            for c in range(0, map_size + 1, step_size):
+                
+                if r < map_size:
+                    landscape[r + half_step][c] = SquareAvgRnd(
+                        landscape, r + half_step, c, half_step, rnd_div)
+                    min_pixel = min(min_pixel, landscape[r + half_step][c])
+                    max_pixel = max(max_pixel, landscape[r + half_step][c])
+                    
+                if c < map_size:
+                    landscape[r][c + half_step] = SquareAvgRnd(
+                        landscape, r, c + half_step, half_step, rnd_div)
+                    min_pixel = min(min_pixel, landscape[r][c + half_step])
+                    max_pixel = max(max_pixel, landscape[r][c + half_step])
+
+        # Prepare the next step.
+        step_size = half_step
+        rnd_div = 2.0 * rnd_div
+
+    delta = max_pixel - min_pixel
+
+    if delta == 0.0:  # rather not realistic, unless the map size is 1
+        delta = 1.0
+
+    for r in range(map_size + 1):
+
+        for c in range(map_size + 1):
+            landscape[r][c] = (landscape[r][c] - min_pixel) / delta
+
+    return landscape
